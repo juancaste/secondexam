0428ff064b13f96b9d2b3ccf8c59d320b49a5b2f
juancaste <juandiegocastellanos7@gmail.com>
2022-10-15 10:09:07 -0500

    text added

--- App.js
+++ App.js
@@ -43,12 +43,12 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Map onLongPress={handleLongPress} puntos={puntos} pointsFilter={pointsFilter} />
-      <Panel onPressLeft={handleLista} textLetf='Lista' togglePointsFilter={togglePointsFilter} />
+      <Panel onPressLeft={handleLista} textLetf='Listar ' togglePointsFilter={togglePointsFilter} />
       <Modal visibility={visibility}>
         { visibilityFilter === 'new_puntos'
           ?
           <View style={styles.form}>
-            <Input title="Nombre" placeholder='Nombre del punto' onChangeText={handleChangeText} />
+            <Input title="Nombre (No caracteres, no numeros)" placeholder='Nombre del punto' onChangeText={handleChangeText} />
             <Button title="Aceptar" onPress={handleSubmit}>
             </Button>
           </View>
